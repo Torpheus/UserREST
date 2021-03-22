322e173bc677e18b2ec2d0e702ebe5199a34f872
Tobias Leichtfried <tobias.leichtfried@sz-ybbs.ac.at>
2021-03-22 14:32:38 +0100

    connect to database

--- server/server.go
+++ server/server.go
@@ -1,30 +1,68 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
-	"sync"
+	"time"
 )
 
 type user struct {
+	Id       int
 	Name     string
 	Email    string
-	Password []byte
+	Password string
 }
 
-var dbLock sync.RWMutex
-var userDb []user
+var database *sql.DB
+var queryUsers *sql.Stmt
+var queryUser *sql.Stmt
+var insertUser *sql.Stmt
 
 func main() {
-	userDb = append(userDb, user{"test", "user", nil})
+
+	database = initDatabase()
+
 	http.HandleFunc("/users/", userAPI)
 	err := http.ListenAndServe("0.0.0.0:8477", nil)
 	if err != nil {
 		log.Fatalln("Failed to start server")
 	}
+
+	_ = queryUsers.Close()
+	_ = queryUser.Close()
+	_ = insertUser.Close()
+
+	_ = database.Close()
+}
+
+func initDatabase() *sql.DB {
+	db, err := sql.Open("mysql", "root:password@tcp(172.17.0.2)/users")
+	if err != nil || db.Ping() != nil {
+		log.Fatalln("Failed to open database")
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	queryUsers, err = db.Prepare("SELECT * FROM users")
+	if err != nil {
+		log.Fatalln("Failed to prepare statement \"SELECT * FROM users\"")
+	}
+	queryUser, err = db.Prepare("SELECT * FROM users WHERE Id = ?")
+	if err != nil {
+		log.Fatalln("Failed to prepare statement \"SELECT * FROM users WHERE Id = ?\"")
+	}
+	insertUser, err = db.Prepare("INSERT INTO users(Name, Email, Password) VALUES (?, ?, ?)")
+	if err != nil {
+		log.Fatalln("Failed to prepare statement \"INSERT INTO users(Name, Email, Password) VALUES (?, ?, ?)\"")
+	}
+	return db
 }
 
 func userAPI(w http.ResponseWriter, req *http.Request) {
@@ -47,40 +85,70 @@ func addUser(req *http.Request) {
 		log.Printf("Server \"PUT /users/\", tried to put content type %q\n", req.Header.Get("Content-Type"))
 		return
 	}
-	decoder := json.NewDecoder(req.Body)
 	var newUser user
-	dbLock.Lock()
+
+	decoder := json.NewDecoder(req.Body)
 	err := decoder.Decode(&newUser)
-	dbLock.Unlock()
+
 	if err != nil {
 		log.Printf("Server \"PUT /users/\", failed to decode json\n")
 		return
 	}
-	userDb = append(userDb, newUser)
+
+	_, err = insertUser.Exec(newUser.Name, newUser.Email, newUser.Password)
+	if err != nil {
+		log.Printf("Inserting of user %v into database failed with %v\n", newUser, err)
+	}
 }
 
 func getUser(path string, w http.ResponseWriter) {
 	id, err := strconv.Atoi(path)
-	if err != nil || len(userDb) < id {
+	if err != nil {
 		log.Printf("Serving \"GET /users/%s\", failed to convert path to id\n", path)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	encode := json.NewEncoder(w)
-	dbLock.RLock()
-	err = encode.Encode(userDb[id])
-	dbLock.RUnlock()
+
+	var selectedUser user
+	row := queryUser.QueryRow(id)
+	err = row.Scan(&selectedUser.Id, &selectedUser.Name, &selectedUser.Email, &selectedUser.Password)
 	if err != nil {
-		log.Printf("Serving \"GET /users/%s\", failed to json encode user\n", path)
+		log.Printf("Fetching user id %d from database failed", id)
+		return
+	}
+	encode := json.NewEncoder(w)
+	err = encode.Encode(selectedUser)
+
+	if err != nil {
+		log.Printf("Serving \"GET /users/%s\", failed to json encode user %d\n", path, id)
 	}
 }
 
 func getAll(w http.ResponseWriter) {
+	rows, err := queryUsers.Query()
+	if err != nil {
+		log.Printf("Querying of all users failed with %v\n", err)
+		return
+	}
+
+	var users []user
+	for rows.Next() {
+		var user user
+		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+		if err != nil {
+			log.Printf("%v\n", err)
+			return
+		}
+		users = append(users, user)
+	}
+	if rows.Err() != nil {
+		log.Printf("%v\n", rows.Err())
+		return
+	}
+
 	encoder := json.NewEncoder(w)
 	w.Header().Set("Content-Type", "application/json")
-	dbLock.RLock()
-	err := encoder.Encode(userDb)
-	dbLock.RUnlock()
+	err = encoder.Encode(users)
 	if err != nil {
 		log.Println("Serving \"GET /users/\", json encode failed")
 	}
